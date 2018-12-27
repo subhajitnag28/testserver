c9e86c65e688e887693f170660ce8d2af87fbde0
Subhajit Nag <subhajit.nag@memeinfotech.com>
2018-12-28 00:16:40 +0530

    socket integrate

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var db = require('./db');
-var cors = require('cors')
-var routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 7000;
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-var port = process.env.PORT || 7000;
+
+const db = require('./db');
+const routes = require('./routes');
+// const router = express.Router();
 
 db.connect('mongodb://test:password1@ds237932.mlab.com:37932/ionic_test', function (err) {
-// db.connect('mongodb://127.0.0.1:27017/app_server', function (err) {
+    // db.connect('mongodb://127.0.0.1:27017/app_server', function (err) {
     if (err) {
         console.log('Unable to connect to Mongo.')
         process.exit(1)
@@ -18,13 +24,20 @@ db.connect('mongodb://test:password1@ds237932.mlab.com:37932/ionic_test', functi
         console.log("connect to mongodb");
     }
 });
-var router = express.Router();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+    console.log("socket connected :", socket.id);
+});
+
 
 app.use(express.static('public'));
 app.use('/image', express.static(__dirname + '/uploadUserImage'));
 app.use('/eventImage', express.static(__dirname + '/eventsImage'));
 app.use('/skillShareImage', express.static(__dirname + '/skillShareImage'));
 app.use('/api', routes);
-app.listen(port);
+server.listen(port);
 
 console.log('Listening on port ' + port);
